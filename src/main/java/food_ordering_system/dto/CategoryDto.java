bd88380c86d0107440737acf59834f3651767f81
Destiny Okeke <Destife10@gmail.com>
2026-06-17 18:12:30 +0200

    feat(category): input validation and validation dependency

--- src/main/java/food_ordering_system/dto/CategoryDto.java
+++ src/main/java/food_ordering_system/dto/CategoryDto.java
@@ -1,9 +1,14 @@
 package food_ordering_system.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CategoryDto {
     private Long id;
+
+    @NotBlank(message = "Category name is required")
+    @Size(min = 2, max = 50, message = "Name must be 2-50 characters")
     private String name;
 }
